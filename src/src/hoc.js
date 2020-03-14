b9644565b879125ff6974379af74cd1aaaa0b764
kusa-mochi <whoatemyapplepie@gmail.com>
2020-03-14 20:15:00 +0900

    fix slot.

--- src/src/hoc.js
+++ src/src/hoc.js
@@ -1,20 +1,20 @@
-export default function(WrappedComponent) {
+export default function (WrappedComponent) {
     return ({
         props: typeof WrappedComponent === 'function'
             ? WrappedComponent.options.props
             : WrappedComponent.props,
         mounted() {
-            console.log('mounted!');
+            alert("Enhanced Component was mounted.");
         },
         render(h) {
+            const slots = this.$slots;
+            const scopedSlots = {};
+            Object.keys(slots).map(key => (scopedSlots[key] = () => slots[key]));
             return h(WrappedComponent, {
                 attrs: this.$attrs,
                 props: this.$props,
                 on: this.$listeners,
-                scopedSlots: {
-                    default: () => this.$slots.default,
-                    test: () => this.$slots.test,
-                }
+                scopedSlots
             });
         }
     });
